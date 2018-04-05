a304a02985406ee408114beee3e80f240bac47e3
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-04-05 16:24:23 +0300

    :sparkles: add graphql functions

--- src/hello_clojure/graphql.clj
+++ src/hello_clojure/graphql.clj
@@ -1,6 +1,6 @@
 (ns hello-clojure.graphql
   (:require [graphql-clj.executor :as executor]
-            [hello-clojure.posts :as posts]))
+            [hello-clojure.comments :as comments]))
 
 ; Load schema from .graphql
 (defn schema
@@ -10,3 +10,35 @@
 ; Schema string
 (def schema-str
   (schema))
+
+; methods
+(defn create-comment [args]
+  (let [body (get args "body")
+        post_id (get args "postId")
+        author (get args "author")]
+    (comments/create-with body post_id author)))
+
+(defn comment [args]
+  (let [id (get args "id")]
+    (comments/find-by-id id)))
+
+(defn comments-by-post [args]
+  (let [post_id (get args "postId")]
+    (comments/where-post post_id)))
+
+(defn comments-by-date [args]
+  (let [post_id (get args "postId")
+        created_on (get args "createdOn")]
+    (comments/where-post-date post_id created_on)))
+
+(defn comments-by-dates [args]
+  (let [post_id (get args "postId")
+        start_date (get args "startDate")
+        end_date (get args "endDate")]
+    (comments/where-post-dates post_id start_date end_date)))
+
+(defn comments-with-limit [args]
+  (let [post_id (get args "postId")
+        limit (get args "limit")
+        page (get args "page")]
+    (comments/where-pagination post_id limit page)))
